cfebef3ce6cb6e02607257dc4aa59e9bacc64d85
Oleg Krasikov <oleg666835@gmail.com>
2025-07-07 02:11:12 +0400

    9.2 pravka import funkciy

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,8 @@
+from .masks import get_mask_account, get_mask_card_number
 from datetime import datetime
 
-account_info = input(str())
-date_string = input(str())
+account_info = input()
+date_string = input()
 
 
 def mask_account_card(account_info: str) -> str:
@@ -9,47 +10,17 @@ def mask_account_card(account_info: str) -> str:
     Маскирует номер карты или счета в переданной строке.
     """
 
-    def mask_card_number(card_number: int) -> str:
-        """
-        Маскирует номер карты, оставляя первые 6 и последние 4 цифры, остальные заменяет на '*'
-        """
-        digits = str(card_number)  # Преобразуем число в строку
-
-        if len(digits) == 16:
-            return f"{digits[:4]} {digits[4:6]} **** {digits[-4:]}"
-        elif 16 < len(digits) <= 19:
-            return f"{digits[:4]} {digits[4:6]} ***** {digits[-4:]}"
-        else:
-            return "Номер карты должен содержать от 16 до 19 цифр"
-
-    def mask_account(account_number: int) -> str:
-        """
-        Маскирует номер счёта, оставляя только последние 4 цифры.
-        """
-        str_number = str(account_number)
-
-        if len(str_number) <= 4:
-            return "Номер счёта должен содержать минимум 4 цифры"
-        masked_part = "**" + str_number[-4:]
-        return masked_part
-
-    # Разделяем строку на части (тип и номер)
     parts = account_info.split()
-
-    # Проверяем, является ли последняя часть номером (содержит только цифры)
+    number_card = int(parts[-1])
     if parts[-1].isdigit():
-        number = int(parts[-1])  # Преобразуем строку в число перед передачей
         account_type = " ".join(parts[:-1])
-        if account_type.lower() == "счет":
-            masked_number = mask_account(number)
+        if parts[0] == "Счет":
+            result = f"{account_type[:-1]}: {get_mask_account(number_card)}"
         else:
-            masked_number = mask_card_number(number)
-        return f"{account_type} {masked_number}"
+            result = f"{account_type[:-1]}: {get_mask_card_number(number_card)}"
+        return result
     else:
         return "Некорректные данные"
-
-
-mask_account_card(account_info)
 
 
 def get_date(date_string: str) -> str:
@@ -62,6 +33,3 @@ def get_date(date_string: str) -> str:
         return dt.strftime("%d.%m.%Y")
     except ValueError as e:
         raise ValueError("Некорректный формат даты") from e
-
-
-get_date(date_string)
